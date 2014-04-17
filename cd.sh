cb50f865ea6f7ddeb945f8ad3aa7cb0c3208fea6
Jeremia Kimelman <jbkimelman@gmail.com>
2014-04-17 16:51:42 -0700

    some ascii art cuz its awesome. also .welcome instead of .prompt - made more sense

--- cd.sh
+++ cd.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 cd () {
   builtin cd "$@"
-  PROMPT_FILES_COUNT=$(ls -a | grep ^.prompt | wc -l)
+  WELCOME_FILES_COUNT=$(ls -a | grep ^.welcome | wc -l)
   README_FILES_COUNT=$(ls -a | grep ^README.md | wc -l)
 
-#  echo "prompt files: $PROMPT_FILES_COUNT"
-#  echo "readme files: $README_FILES_COUNT"
-
-  if [ $PROMPT_FILES_COUNT -eq 1 ]
+  if [ $WELCOME_FILES_COUNT -eq 1 ]
   then
-    echo '.prompt: '
-    cat .prompt
+    cat .welcome
   elif [ $README_FILES_COUNT -eq 1 ]
   then
-    echo 'README.md: '
     more README.md
   fi
 }
